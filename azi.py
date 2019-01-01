457dd6cec7c9d10f62181b896034230af48ca625
Alan Baines <abaines@vt.edu>
2019-01-01 15:48:18 -0500

    try catch errors

--- azi.py
+++ azi.py
@@ -257,7 +257,14 @@ def scan():
 
 
 def scanThreader():
-   scan()   
+
+   try:
+      scan()
+   except:
+      exc_type, exc_value, exc_traceback = sys.exc_info()
+      lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+      print (''.join('!! ' + line for line in lines) ) # Log it or whatever here
+     
    t = threading.Timer(scanDelay, scanThreader)
    t.start()
 

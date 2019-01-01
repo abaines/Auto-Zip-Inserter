08665661429cca3b013e94fd5427a939acb67515
Alan Baines <abaines@vt.edu>
2019-01-01 17:16:36 -0500

    infinity_chests_order

--- saves/workingSquares/kizrak.lua
+++ saves/workingSquares/kizrak.lua
@@ -1,11 +1,6 @@
 -- Kizrak
 
 local json = require 'utils.json'
-
-
-
-
-
 local Event = require 'utils.event'
 
 local function on_player_changed_position(event)
@@ -20,12 +15,36 @@ local function setSpeed(event)
 	game.print("Set game speed to " .. parsedSpeed)
 end
 
+if not global.infinity_chests_spawned then
+	global.infinity_chests_spawned = {}
+end
 
+local infinity_chests_order = {
+	{"iron-ore"},
+	{"copper-ore"},
+	{"stone"},
+	{"raw-wood"},
+	{"coal"},
+	{"coal"},
+	{"stone"},
+	{"stone"},
+	{"iron-ore"},
+	{"iron-ore"},
+	{"iron-ore"},
+	{"copper-ore"},
+	{"copper-ore"},
+	{"iron-plate"},
+	{"copper-plate"},
+	{"stone-brick"},
+	{"iron-gear-wheel"},
+	{"copper-cable"}
+}
 
 
 local function playground(event)
 	game.print("play")
-	game.write_file("playground.json",json.stringify(global))
+	game.write_file("global.json",json.stringify(global))
+	game.print(json.stringify(infinity_chests_order))
 end
 
 commands.add_command("speed", "Set game speed", setSpeed)

0912fbeb5cf60a1833932906874bf4b0b42bd0f7
Alan Baines <abaines@vt.edu>
2019-01-01 17:00:11 -0500

    write table as json to file

--- saves/workingSquares/kizrak.lua
+++ saves/workingSquares/kizrak.lua
@@ -1,5 +1,11 @@
 -- Kizrak
 
+local json = require 'utils.json'
+
+
+
+
+
 local Event = require 'utils.event'
 
 local function on_player_changed_position(event)
@@ -14,6 +20,16 @@ local function setSpeed(event)
 	game.print("Set game speed to " .. parsedSpeed)
 end
 
+
+
+
+local function playground(event)
+	game.print("play")
+	game.write_file("playground.json",json.stringify(global))
+end
+
 commands.add_command("speed", "Set game speed", setSpeed)
+
+commands.add_command("play", "Playground",playground)
 
 Event.add(defines.events.on_player_changed_position, on_player_changed_position)

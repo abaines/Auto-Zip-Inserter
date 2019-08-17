d052e83d1ad3078b0f54828d2a1143a5c9d2d5c3
Alan Baines <abaines@vt.edu>
2019-08-17 12:21:13 -0400

    updates for apply bonuses
    new command!
    /bonus

--- saves/drop-in-ready/playerBonuses.lua
+++ saves/drop-in-ready/playerBonuses.lua
@@ -20,8 +20,13 @@ function apply_bonuses()
 		player.game_view_settings.show_rail_block_visualisation = true
 	end
 
-	print("apply_bonuses")
-	log("apply_bonuses")
+	local numberPlayers = #game.players
+	local msg = "apply_bonuses complete: " .. numberPlayers
+	print(msg)
+	log(msg)
+	if not game.is_multiplayer() then
+		game.print(msg,{r=255,g=255})
+	end
 end
 
 script.on_init(apply_bonuses)
@@ -29,4 +34,8 @@ script.on_init(apply_bonuses)
 script.on_event({
 defines.events.on_player_joined_game,
 defines.events.on_player_created,
+defines.on_player_respawned,
 },apply_bonuses)
+
+commands.add_command("bonus", "bonus", apply_bonuses)
+

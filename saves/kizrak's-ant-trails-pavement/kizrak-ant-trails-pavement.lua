d826ed0e963b20dbe64ebe5af7f7b52c77f37ec6
Alan Baines <abaines.vt+github@gmail.com>
2022-09-02 13:39:46 -0400

    auto-formatting

--- saves/kizrak's-ant-trails-pavement/kizrak-ant-trails-pavement.lua
+++ saves/kizrak's-ant-trails-pavement/kizrak-ant-trails-pavement.lua
@@ -1,16 +1,14 @@
 -- Kizrak
+-- kizrak-ant-trails-pavement.lua
+log("kizrak-ant-trails-pavement.lua")
 
 local sb = serpent.block -- luacheck: ignore 211
 
-log("kizrak-ant-trails-pavement.lua")
-
-
 --- TODO: time for a library soon...
 local function sbs(obj) -- luacheck: ignore 211
-	local s = sb( obj ):gsub("%s+", " ")
-	return s
+    local s = sb(obj):gsub("%s+", " ")
+    return s
 end
-
 
 local land_tiles = {}
 
@@ -39,92 +37,79 @@ land_tiles['red-desert-1'] = true
 land_tiles['red-desert-2'] = true
 land_tiles['red-desert-3'] = true
 
-
-
 local ant_trails_pavement = {}
 
+local surface_set_tile = function(surface, position, tile_name)
 
-local surface_set_tile = function(surface,position,tile_name)
+    local tile_array = {{
+        position = position,
+        name = tile_name
+    }}
 
-	local tile_array = {{
-		position = position,
-		name=tile_name,
-	}}
+    local correct_tiles = false
+    local remove_colliding_entities = "abort_on_collision"
+    local remove_colliding_decoratives = true
+    local raise_event = true
 
-	local correct_tiles = false
-	local remove_colliding_entities = "abort_on_collision"
-	local remove_colliding_decoratives = true
-	local raise_event = true
+    local result = surface.set_tiles(tile_array, correct_tiles, remove_colliding_entities, remove_colliding_decoratives,
+        raise_event)
 
-	local result = surface.set_tiles(
-		tile_array,
-		correct_tiles,
-		remove_colliding_entities,
-		remove_colliding_decoratives,
-		raise_event
-	)
-
-	return result
+    return result
 end
 
-
 local place_stone_from_player = function(player)
-	local position =player.position
-	local surface = player.surface
+    local position = player.position
+    local surface = player.surface
 
-	local inventory = player.get_main_inventory()
-	local count_stone_brick = inventory.get_item_count("stone-brick")
-	
-	log("count_stone_brick = " .. count_stone_brick)
+    local inventory = player.get_main_inventory()
+    local count_stone_brick = inventory.get_item_count("stone-brick")
 
-	if count_stone_brick > 100 then
-		inventory.remove({
-			name="stone-brick",
-			count=1,
-		})
+    log("count_stone_brick = " .. count_stone_brick)
 
-		local result = surface_set_tile(surface, position, "stone-path")
+    if count_stone_brick > 100 then
+        inventory.remove({
+            name = "stone-brick",
+            count = 1
+        })
 
-		if result ~= nil then
-			log("💣")
-			log(result)
-		end
-	end
+        local result = surface_set_tile(surface, position, "stone-path")
+
+        if result ~= nil then
+            log("💣")
+            log(result)
+        end
+    end
 end
 
 local on_player_changed_position = function(event)
-	local player = game.players[event.player_index]
-	local position =player.position
-	log('§' .. ' on_player_changed_position ' .. sbs(position))
+    local player = game.players[event.player_index]
+    local position = player.position
+    log('§' .. ' on_player_changed_position ' .. sbs(position))
 
-	local surface = player.surface
-	local tile = surface.get_tile(position)
-	local tile_name = tile.name
+    local surface = player.surface
+    local tile = surface.get_tile(position)
+    local tile_name = tile.name
 
-	if land_tiles[tile_name] then
-		log("land! = " .. tile_name)
-		place_stone_from_player(player)
-	else
-		log("invalid-tile = " ..tile_name)
-	end
+    if land_tiles[tile_name] then
+        log("land! = " .. tile_name)
+        place_stone_from_player(player)
+    else
+        log("invalid-tile = " .. tile_name)
+    end
 
 end
 
-
-
-ant_trails_pavement.events =
-{
-	[defines.events.on_player_changed_position] = on_player_changed_position
+ant_trails_pavement.events = {
+    [defines.events.on_player_changed_position] = on_player_changed_position
 }
 
 ant_trails_pavement.on_configuration_changed = function()
-	log("on_configuration_changed")
+    log("on_configuration_changed")
 end
 
 ant_trails_pavement.on_init = function()
-	log("on_init")
+    log("on_init")
 end
-
 
 return ant_trails_pavement
 

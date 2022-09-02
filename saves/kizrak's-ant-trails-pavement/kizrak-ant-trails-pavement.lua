384842bb2fc55bd13c0640e7b22764636e249783
Alan Baines <abaines.vt+github@gmail.com>
2022-09-02 13:54:34 -0400

    Update kizrak-ant-trails-pavement.lua
    cleanup names; doc; remove dead code;

--- saves/kizrak's-ant-trails-pavement/kizrak-ant-trails-pavement.lua
+++ saves/kizrak's-ant-trails-pavement/kizrak-ant-trails-pavement.lua
@@ -6,8 +6,7 @@ local sb = serpent.block -- luacheck: ignore 211
 
 --- TODO: time for a library soon...
 local function sbs(obj) -- luacheck: ignore 211
-    local s = sb(obj):gsub("%s+", " ")
-    return s
+    return sb(obj):gsub("%s+", " ")
 end
 
 local land_tiles = {}
@@ -41,7 +40,7 @@ local ant_trails_pavement = {}
 
 local surface_set_tile = function(surface, position, tile_name)
 
-    local tile_array = {{
+    local tiles = {{
         position = position,
         name = tile_name
     }}
@@ -51,7 +50,8 @@ local surface_set_tile = function(surface, position, tile_name)
     local remove_colliding_decoratives = true
     local raise_event = true
 
-    local result = surface.set_tiles(tile_array, correct_tiles, remove_colliding_entities, remove_colliding_decoratives,
+    -- https://lua-api.factorio.com/latest/LuaSurface.html#LuaSurface.set_tiles
+    local result = surface.set_tiles(tiles, correct_tiles, remove_colliding_entities, remove_colliding_decoratives,
         raise_event)
 
     return result
@@ -85,17 +85,13 @@ end
 local on_player_changed_position = function(event)
     local player = game.players[event.player_index]
     local position = player.position
-    -- log('§' .. ' on_player_changed_position ' .. sbs(position))
 
     local surface = player.surface
     local tile = surface.get_tile(position)
     local tile_name = tile.name
 
     if land_tiles[tile_name] then
-        -- log("land! = " .. tile_name)
         place_stone_from_player(player)
-    else
-        -- log("invalid-tile = " .. tile_name)
     end
 
 end
@@ -103,14 +99,6 @@ end
 ant_trails_pavement.events = {
     [defines.events.on_player_changed_position] = on_player_changed_position
 }
-
-ant_trails_pavement.on_configuration_changed = function()
-    log("on_configuration_changed")
-end
-
-ant_trails_pavement.on_init = function()
-    log("on_init")
-end
 
 return ant_trails_pavement
 

c10722db39eca39a3010a5f2ece68ee7d656f090
Alan Baines <abaines.vt+github@gmail.com>
2022-09-02 12:25:19 -0400

    check for count of stone-brick and remove
    get position

--- saves/kizrak's-ant-trails-pavement/kizrak-ant-trails-pavement.lua
+++ saves/kizrak's-ant-trails-pavement/kizrak-ant-trails-pavement.lua
@@ -11,8 +11,24 @@ local on_player_changed_position = function(event)
 	log('on_player_changed_position')
 	log(sb(event))
 	local player = game.players[event.player_index]
+	local position =player.position
+	log(position)
 	
+
+	inventory = player.get_main_inventory()
+	count_stone_brick = inventory.get_item_count("stone-brick")
+	log(count_stone_brick)
+
+	if count_stone_brick > 100 then
+
+		inventory.remove({
+			name="stone-brick",
+			count=1,
+		})
+	end
 end
+
+
 
 ant_trails_pavement.events =
 {
@@ -27,7 +43,6 @@ ant_trails_pavement.on_init = function()
 	log("on_init")
 end
 
--- script.on_event(defines.events.on_player_changed_position, on_player_changed_position)
-
 
 return ant_trails_pavement
+
